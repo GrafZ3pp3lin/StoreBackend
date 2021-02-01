1439838e2f21c4827ae6c7e88eac9d659d7e5140
GrafZeppelin <grafzeppelin@gamlo.de>
2021-02-01 22:49:41 +0100

    add cors

--- StoreBackend/Startup.cs
+++ StoreBackend/Startup.cs
@@ -21,6 +21,14 @@ namespace Gamlo.StoreBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithMethods("PUT", "GET")
+                        .AllowAnyOrigin();
+                });
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -42,6 +50,8 @@ namespace Gamlo.StoreBackend
             }
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
